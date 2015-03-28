df3fe9614da8231b0650dcc705da19e30764de31
kennethms64 <kennethms64@gmail.com>
2015-03-28 17:43:57 -0600

    Estructuras de control basicas

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "queue.h"
+ 
+int main()
+{
+	printf("----------------Testing Process----------------\n");
+	struct process *p1 = new_process(1,8,1);
+	print_data(p1);
+	update_state(p1,2);
+	print_data(p1);
+	printf("-----------------------------------------------\n");
+	
+	printf("-----------------Testing Queue-----------------\n");
+	create();
+	enq(1,8,2);
+	enq(2,6,1);
+	enq(3,11,4);
+	enq(4,9,3);
+	
+	display();	
+	queuesize();
+	
+	deq();
+	display();	
+	queuesize();
+	
+	return 0;
+}
---
+++ src/queue.h
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "process.h"
+ 
+struct queue
+{
+    struct process *current;
+    struct queue *next;
+}*front,*rear,*temp,*front1;
+ 
+int count = 0;
+ 
+/* Create an empty queue */
+void create()
+{
+    front = rear = NULL;
+}
+ 
+/* Returns queue size */
+void queuesize()
+{
+    printf("\n-------------Queue size : %d-------------", count);
+}
+ 
+/* Enqueing the queue */
+void enq(int pId,int pBurst,int pPriority)
+{
+    if (rear == NULL)
+    {
+    	front = (struct queue *)malloc(sizeof(struct queue));
+        rear = (struct queue *)malloc(sizeof(struct queue));
+        rear->next = NULL;
+        rear->current = new_process(pId,pBurst,pPriority);
+        front = rear;
+    }
+    else
+    {
+        temp=(struct queue *)malloc(sizeof(struct queue));
+        temp->next = NULL;
+        rear->next = temp;
+        temp->current = new_process(pId,pBurst,pPriority);
+        rear = temp;
+    }
+    count++;
+}
+
+/* Dequeing the queue */
+void deq()
+{
+    front1 = front;
+ 
+    if (front1 == NULL)
+    {
+        printf("\n Error: Trying to display elements from empty queue");
+        return;
+    }
+    else
+        if (front1->next != NULL)
+        {
+            front1 = front1->next;
+            printf("\nDequed value : %d\n\n", front->current->id);
+            
+            free(front);
+            front = front1;
+        }
+        else
+        {
+            printf("\nDequed value : %d\n\n", front->current->id);
+            
+            free(front);
+            front = NULL;
+            rear = NULL;
+        }
+        count--;
+}
+
+/* Displaying the queue elements */
+void display()
+{
+    front1 = front;
+ 
+    if ((front1 == NULL) && (rear == NULL))
+    {
+        printf("Queue is empty");
+        return;
+    }
+    while (front1 != rear)
+    {
+        print_data(front1->current);
+        front1 = front1->next;
+    }
+    if (front1 == rear)
+        print_data(front1->current);
+}
