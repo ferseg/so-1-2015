0b80b9de45ce5dc6166fdfd2e7048c4d351ba2be
kennethms64 <kennethms64@gmail.com>
2015-03-29 13:24:24 -0600

    Creacion de paquetes

---
+++ Headers/process.h
@@ -0,0 +1,28 @@
+#ifndef PROCESS_H_   /* Include guard */
+#define PROCESS_H_
+
+struct process 
+{
+    int id;
+    int burst;
+    int state;
+    int priority;
+};
+
+struct process* new_process(int pId, int pBurst,int pPriority);
+
+int get_id(struct process *pProcess);
+
+int get_burst(struct process *pProcess);
+
+int get_state(struct process *pProcess);
+
+int get_priority(struct process *pProcess);
+
+void update_state(struct process *pProcess,int pValue);
+
+const char * to_string(struct process *pProcess);
+
+void print_data(struct process *pProcess);
+
+#endif // PROCESS_H_
